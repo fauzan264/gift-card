b80c8acaae63c1fe2cf51e621269f909b43dee39
Fauzan <fauzan.c4@gmail.com>
2025-04-10 22:58:55 +0700

    create component gift form

---
+++ src/components/GiftForm.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(['form'])
+</script>
+
+<template>
+    <div class="space-y-2">
+        <input v-model="form.dear" placeholder="Dear" class="block w-full border p-2 rounded" />
+        <textarea v-model="form.message" placeholder="Message" class="block w-full border p-2 rounded" />
+        <input v-model="form.from" placeholder="From" class="block w-full border p-2 rounded" />
+    </div>
+</template>
+
+<style scoped>
+</style>
