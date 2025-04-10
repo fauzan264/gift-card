89edc8975d4a8e361348b03929465e9f4c3c34da
Fauzan <fauzan.c4@gmail.com>
2025-04-10 22:59:35 +0700

    create component download button

---
+++ src/components/DownloadButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+const emit = defineEmits(['download'])
+</script>
+
+<template>
+    <div class="flex justify-center">
+        <button @click="$emit('download')" class="bg-green-500 text-white w-50 px-8 py-2 rounded hover:bg-green-600">
+            Download
+        </button>
+    </div>
+</template>
