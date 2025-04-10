42cf6fbccd5d510b562fd1e892b7d141fad687b0
Fauzan <fauzan.c4@gmail.com>
2025-04-10 22:59:05 +0700

    create component file upload

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,15 @@
+<script setup>
+const emit = defineEmits(['file-selected'])
+
+const onChange = (e) => {
+    const file = e.target.files[0]
+    if (file) emit('file-selected', file)
+}
+</script>
+
+<template>
+    <input type="file" @change="onChange" accept="image/*" class="block w-full border p-2 rounded" />
+</template>
+
+<style scoped>
+</style>
